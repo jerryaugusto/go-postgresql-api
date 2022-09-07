053f929ffed023402ba709adda279fa7e5ae18cf
Jerry Augusto <jerryaugustodev@gmail.com>
2022-09-07 11:13:34 -0400

    First commit.

---
+++ configs/config.go
@@ -0,0 +1,30 @@
+package configs
+
+var cfg *config
+
+type config struct {
+	API APIConfig
+	DB  DBConfig
+}
+
+type APIConfig struct {
+	Port string
+}
+
+type DBConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Pass     string
+	Database string
+}
+
+func init() {
+	viper.SetDefault("api.port", "9000")
+	viper.SetDefault("database.host", "localhost")
+	viper.SetDefault("database.port", "5432")
+}
+
+func Load() error {
+	viper.SetConfigName()
+}
